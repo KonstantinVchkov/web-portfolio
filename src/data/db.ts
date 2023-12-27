2f807700c98428fbb894e1cd84c86f25cabffc0e
Konstantin <kvckov@yahoo.com>
2023-12-27 17:35:41 +0100

    data added inside the db.ts for the projects section

--- src/data/db.ts
+++ src/data/db.ts
@@ -28,3 +28,81 @@ export const contact = {
   linkedIn: "https://www.linkedin.com/in/konstantin-vchkov-b39200210/",
   instagram: "https://www.instagram.com/konstantintinko/",
 };
+export const projectsData = [
+  {
+    name: "Brainster Labs",
+    tech: ["HTML", "CSS", "JAVASCRIPT", "Bootstrap"],
+    description:
+      "Brainster Labs is a project on which we will place all the projects Brainster students (Design Academy, Coding Academy, Marketing Academy) have made.",
+    image: "/assets/brainster-labs-image.png",
+    linkCode: "https://github.com/KonstantinVchkov/Brainster-Labs",
+    // showLive: "https://street-artist-brainster-project.netlify.app/",
+  },
+  {
+    name: "Street Artists",
+    tech: ["HTML", "CSS", "BOOTSTRAP", "JAVASCRIPT"],
+    description:
+      "Mobile web space for every street artist that will allow them to be able to track their income, and to have a place on the web where they can showcase their masterpieces.",
+    image: "/assets/street-artist-image.png",
+    linkCode: "https://github.com/KonstantinVchkov/Street-Artists",
+    showLive: "https://street-artist-brainster-project.netlify.app/",
+  },
+  {
+    name: "E-commerce",
+    tech: ["React.Js", "SASS", "Styled Components", "Redux", "Firebase"],
+    description:
+      "Welcome to our state-of-the-art e-commerce app, the ultimate destination for fashion enthusiasts! Our platform is specifically designed to cater to your style needs, offering a wide range of trendy and high-quality products.With our intuitive and user-friendly interface, shopping is a breeze. Enjoy a seamless journey from browsing to checkout, with secure payment options and fast shipping. Our e-commerce app is more than just a shopping platform; it's a fashion haven where quality, style, and convenience converge.",
+    image: "/assets/e-commerce.png",
+    linkCode: "https://github.com/KonstantinVchkov/E-Commerce",
+  },
+  {
+    name: "Igralishte.Sk",
+    tech: [
+      "Next.Js",
+      "CSS",
+      "Bootstrap",
+      "Node.Js",
+      "Typescript",
+      "React-Hook-Form",
+      "Yup",
+    ],
+    description:
+      "Created an online platform for a vintage women's clothing store using NextJS and jsonserver, incorporating e-commerce features and a mock REST API. This project, part ofmy academic work, combined aesthetic design with functional back-end structures using NodeJS and related packages.",
+    image: "/assets/brainster-labs-image.png",
+    link: "https://github.com/KonstantinVchkov/igralishte",
+    showLive: "https://igralishte.vercel.app/",
+  },
+  {
+    name: "AIR CARE HACKATHON for Breathe.mk",
+    tech: [
+      "Next.Js",
+      "CSS",
+      "Bootstrap",
+      "Formik",
+      "Typescript",
+      "Leaflet",
+      "Yup",
+      "React-webcam,",
+      "UUID",
+    ],
+    description:
+      "Contributed to a multi-disciplinary team developing an app for identifying air pollution hotspots, aimed at promoting environmental consciousness. My role focused on designing responsive user interfaces with Next.js, ensuring seamless integration with      back-end systems. The app successfully heightened public awareness about air pollution.",
+    image: "/assets/brainster-labs-image.png",
+    link: "https://github.com/KonstantinVchkov/air-care",
+  },
+  {
+    name: "Marinov Design",
+    tech: [
+      "Next.Js",
+      "CSS",
+      "Bootstrap",
+      "Typescript",
+      "lottie-web",
+      "Axios"
+    ],
+    description:
+      "Contributed to a multi-disciplinary team developing an app for identifying air pollution hotspots, aimed at promoting environmental consciousness. My role focused on designing responsive user interfaces with Next.js, ensuring seamless integration with      back-end systems. The app successfully heightened public awareness about air pollution.",
+    image: "/assets/brainster-labs-image.png",
+    link: "https://github.com/KonstantinVchkov/air-care",
+  },
+];
