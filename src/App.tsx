f40a524750aeece7224bbc34d65b7c333fc11d2b
Konstantin <kvckov@yahoo.com>
2023-12-27 09:53:21 +0100

    framer motion installed,css changed,image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import Navbar from "./components/HeaderComponent/Navbar";
 import Home from "./components/HomeComponent/Home";
-import './App.css'
+import "./App.css";
 import About from "./components/AboutComponent/About";
+import { motion, useScroll, useSpring } from "framer-motion";
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <div className="App">
+      <motion.div className="progress-bar" style={{ scaleX }} />
+
       <Navbar />
       <Home />
       <About />
--- src/components/AboutComponent/About.tsx
+++ src/components/AboutComponent/About.tsx
@@ -1,47 +1,47 @@
-import './about.css'
+import { Parallax, useParallax } from "react-scroll-parallax";
+import "./about.css";
 
 const About = () => {
   return (
-    <section id="about">
-    <div className="container">
-      <h2 className="section-title load-hidden">About me</h2>
-      <div className="row about-wrapper">
-        <div className="col-md-6 col-sm-12">
-          <div className="about-wrapper__image load-hidden">
-            <img
-              alt="Profile Image"
-              className="img-fluid rounded shadow-lg"
-              height="auto"
-              width="300px"
-              src="assets/profile.jpg"
-            />
+    <div id="about">
+      <div className="container d-flex justify-content-center align-center flex-column">
+        <h2 className="section-title load-hidden">About me</h2>
+        <div className="row about-wrapper d-flex justify-content-center">
+          <div className="col-md-6 col-sm-12">
+            <div className="about-wrapper__image load-hidden">
+              <img
+                alt="Profile Image"
+                className="img-fluid rounded shadow-lg"
+                height="auto"
+                width="300px"
+                src="/assets/self-image.jpg"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-md-6 col-sm-12">
-          <div className="about-wrapper__info load-hidden">
-            <p className="about-wrapper__info-text">
-              This is where you can describe about yourself. The more you
-              describe about yourself, the more chances you can!
-            </p>
-            <p className="about-wrapper__info-text">
-              Extra Information about you! like hobbies and your goals.
-            </p>
-            <span className="d-flex mt-3">
-              <a
-                rel="noreferrer"
-                target="_blank"
-                className="cta-btn cta-btn--resume"
-                href="assets/resume.pdf"
-              >
-                View Resume
-              </a>
-            </span>
+          <div className="col-md-6 col-sm-12">
+            <div className="about-wrapper__info load-hidden">
+              <p className="about-wrapper__info-text">
+                    
+              </p>
+              <p className="about-wrapper__info-text">
+             
+              </p>
+              <span className="d-flex mt-3">
+                <a
+                  rel="noreferrer"
+                  target="_blank"
+                  className="cta-btn cta-btn--resume"
+                  href="assets/resume.pdf"
+                >
+                  View Resume
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
