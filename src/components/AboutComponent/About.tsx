3084db3c0c29991785963c22bc70939acb2d01f5
Konstantin Vckov <kvckov@yahoo.com>
2023-12-30 21:34:51 +0100

    navbar fixed,about style fixed,img deleted,button style on about section changed

--- src/components/AboutComponent/About.tsx
+++ src/components/AboutComponent/About.tsx
@@ -9,29 +9,15 @@ const About = ({ name, lastname, aboutme }: AboutMe) => {
     <div id="about">
       <div className="container d-flex justify-content-center align-center flex-column">
         <h2 className="section-title load-hidden">About me</h2>
-        <div className="row about-wrapper d-flex justify-content-center">
-          <div className="col-md-6 col-sm-12">
-            <div className="about-wrapper__image load-hidden">
-              <img
-                alt="Profile Image"
-                className="img-fluid rounded shadow-lg"
-                height="auto"
-                width="300px"
-                src="/assets/self-image.png"
-              />
-            </div>
-          </div>
+        <div className="row about-wrapper">
           <div className="col-md-6 col-sm-12">
             <div className="about-wrapper__info load-hidden">
-              <h1>
-                {name} {lastname}
-              </h1>
               <p className="about-wrapper__info-text">{aboutme}</p>
               <span className="d-flex mt-3">
                 <a
                   rel="noreferrer"
                   target="_blank"
-                  className="cta-btn cta-btn--resume"
+                  className="cta-btn cta-btn--hero"
                   href={"/assets/KonstantinVchkov_CV.pdf"}
                 >
                   View Resume
