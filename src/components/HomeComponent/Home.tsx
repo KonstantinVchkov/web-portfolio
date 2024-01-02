07791c7239752c8dc98c55f57ea28590f787dc8d
Konstantin <kvckov@yahoo.com>
2024-01-02 17:36:23 +0100

    ss

--- src/components/HomeComponent/Home.tsx
+++ src/components/HomeComponent/Home.tsx
@@ -1,24 +1,40 @@
 import "./style.css";
 import "../styles/buttonStyle.css";
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter } from "react-simple-typewriter";
+import { Link } from "react-scroll";
 
 // import Typewriter from "typewriter-effect";
 
 const Home = () => {
-  const sentences = ["Front-End Developer", "React Developer",'Next js Developer'];
+  const sentences = [
+    "Front-End Developer",
+    "React Developer",
+    "Next js Developer",
+  ];
 
   return (
     <section id="hero" className="jumbotron">
       <div className="container">
         <h1 className="hero-title load-hidden">
-          Hi There, I'm <span className="text-color-main">Konstantin Vchkov</span>
+          Hi There, I'm{" "}
+          <span className="text-color-main">Konstantin Vchkov</span>
           <br />
           <Typewriter loop={true} cursor words={sentences} />
         </h1>
         <p className="hero-cta load-hidden">
-          <a rel="noreferrer" className="cta-btn cta-btn--hero" href="#about">
+          <Link
+            smooth={true}
+            duration={500}
+            spy={true}
+            offset={-70}
+            to={"about"}
+            className="cta-btn cta-btn--hero"
+          >
             Know more about me
-          </a>
+          </Link>
+          {/* <a rel="noreferrer"  href="#about">
+           
+          </a> */}
         </p>
       </div>
     </section>
