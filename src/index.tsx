089f93626fb9cfb849f5ceb95cc5f3af61e24cca
Konstantin <kvckov@yahoo.com>
2024-01-11 21:40:23 +0100

    analytics added

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
 
 reportWebVitals();
