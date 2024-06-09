7165d33eedf1643910c6a0a056d687ff8c5b483b
Konstantin Vckov <kvckov@yahoo.com>
2024-06-09 10:14:50 +0200

    updated site key spelling name

--- src/components/ContactComponent/Contact.tsx
+++ src/components/ContactComponent/Contact.tsx
@@ -6,7 +6,7 @@ import useContactForm from "../../hooks/useContactForm";
 
 export const ContactUs = () => {
   const { setCaptVal, email, sendEmail, form, captVal } = useContactForm();
-  const env = process.env.GOOGLE_RECAPTCHA_KEY
+  const RECAPTCHA_KEY = process.env.REACT_APP_SITE_RECAPTCHA_KEY || "";
   return (
     <div id="contact" className="ContactForm">
       <div className="Animation">
@@ -44,7 +44,7 @@ export const ContactUs = () => {
             name="message"
           />
           <ReCAPTCHA
-            sitekey={`${env}`}
+            sitekey={RECAPTCHA_KEY}
             onChange={(val: string | null) => setCaptVal(val)}
           />
           <input
